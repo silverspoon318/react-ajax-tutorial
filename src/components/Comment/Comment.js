04d489ecc8b27052d7060d076878e6107fd16e46
sungwonkim <sungwon3708@gmail.com>
2017-06-06 20:44:28 +0900

    first commit

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = ({name, body}) => {
+  return (
+    <li className = "Comment">
+      <p>
+        <b>
+          {name}
+        </b>
+        {body}
+      </p>
+    </li>
+  );
+};
+
+export default Comment;
